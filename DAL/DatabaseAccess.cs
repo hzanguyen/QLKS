adc94de838c8a0120dbe521b96502eef31a38fec
ten-tai-khoan <hzanguyen1804@gmail.com>
2023-04-13 17:18:14 +0700

    new commit

---
+++ DAL/DatabaseAccess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using System.Data;
+
+namespace DAL
+{
+
+    public class DatabaseAccess
+    {
+        public static string CheckLogicDTO(TaiKhoan taikhoan)
+        {
+            SqlConnection cnn;
+            cnn = new SqlConnection("Data Source=DESKTOP-UIRD8ES\\MAYCHU;Initial Catalog=QLKS;Integrated Security=True");
+            SqlDataAdapter tkhoan = new SqlDataAdapter("select TaiKhoan,Password from DangNhap where TaiKhoan='" + taikhoan.sTaiKhoan+ "'", cnn);
+            DataSet admin = new DataSet();
+            tkhoan.Fill(admin);
+            
+                string s = "";
+                foreach (DataRow dr in admin.Tables[0].Rows)
+                    s = dr[1].ToString();
+                if (taikhoan.sMatKhau != s.Trim())
+            {
+                return "Tài khoản hoặc mật khẩu không chính xác!";
+            }
+            return taikhoan.sTaiKhoan;
+        }
+    }
+}
---
+++ DTO/TaiKhoan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class TaiKhoan
+    {
+     
+        public string sTaiKhoan { get; set; }
+        public string sMatKhau { get; set; }
+    }
+}
